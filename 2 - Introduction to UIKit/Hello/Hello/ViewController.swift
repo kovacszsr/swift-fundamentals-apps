93f3a050df5a2c0f81702e7ddfc3efb2f809486a
kovacszsr <kovacs.zsofia.ramona98@gmail.com>
2023-11-10 16:25:22 +0100

    Finished "Hello" Lab

---
+++ 2 - Introduction to UIKit/Hello/Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Kovács Zsófia Ramóna on 10/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
